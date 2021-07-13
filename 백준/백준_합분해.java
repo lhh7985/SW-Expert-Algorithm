eaa479618b1f21ea9f23fff7ca738df325f53fa8
HOHWANG <ghghkd52@naver.com>
2021-07-13 22:09:37 +0900

    백준 DP 문제

---
+++ 백준/백준_합분해.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class 백준_합분해 {
+
+	static long dp[][] = new long[201][201];
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+
+		for (int i = 0; i < 201; i++) {
+			dp[i][0] = 1;
+			dp[1][i] = 1;
+		}
+		for (int i = 1; i < 201; i++) {
+			dp[2][i] = i + 1;
+		}
+		for (int i = 3; i < 201; i++) {
+			for (int j = 1; j < 201; j++) {
+				for (int k1 = 0; k1 <= j; k1++) {
+					dp[i][j] += dp[i - 1][j - k1] % 1000000000;
+				}
+			}
+		}
+		System.out.println(dp[k][n] % 1000000000);
+
+	}
+
+}
