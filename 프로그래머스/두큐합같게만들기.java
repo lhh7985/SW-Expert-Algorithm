5822740dd918edc08d8f2692ec322c84a7b95411
lhh7985 <ghghkd52@naver.com>
2022-08-24 00:07:17 +0900

    풀이 1. 각 배열의 합을 구한다. 2. 두 배열 중 큰 곳에서 pop하여 작은 곳에 add한다. 3. 두 배열의 합이 같아질 때 까지 반복한다. 4. pop과 add를 할 때 카운트를 세어주면 정답
    - while문 탈출 조건을 잘 찾아야 한다.

--- 프로그래머스/두큐합같게만들기.java
+++ 프로그래머스/두큐합같게만들기.java
@@ -1,13 +1,27 @@
-class Solution {
-    public int solution(int[] queue1, int[] queue2) {
-        
+package 프로그래머스;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class 두큐합같게만들기 {
+
+    public static void main(String[] args) {
+        int[] queue1 = {};
+        int[] queue2 = {};
+
+        solution(queue1, queue2);
+
+    }
+
+    public static int solution(int[] queue1, int[] queue2) {
+
         long sumQueue1 = 0, sumQueue2 = 0;
         long halfNum = 0;
         Queue<Integer> realQueue1 = new LinkedList<>();
         Queue<Integer> realQueue2 = new LinkedList<>();
-        
+
         int count = 0;
-        
+
         for(int a : queue1){
             sumQueue1 += a;
             realQueue1.add(a);
@@ -16,46 +30,46 @@ class Solution {
             sumQueue2 += b;
             realQueue2.add(b);
         }
-        
+
         halfNum = (sumQueue1 + sumQueue2) / 2;
-        
+
         if((sumQueue1 + sumQueue2) % 2 != 0){
             return -1;
         }
-        
+
         int length = queue1.length;
-        
+
         while(true){
             if(count > (length+length)*2) {
                 return -1;
             }
             if(sumQueue1 > sumQueue2){
                 int pop = realQueue1.poll();
-                
+
                 if(pop == sumQueue1 && pop > halfNum){
                     return -1;
                 }
-                
+
                 realQueue2.add(pop);
-                
+
                 sumQueue1 -= pop;
                 sumQueue2 += pop;
-                
+
                 count++;
-                
+
             }else if (sumQueue2 > sumQueue1){
-                
+
                 int pop = realQueue2.poll();
-                
+
                 if(pop == sumQueue2 && pop > halfNum){
                     return -1;
                 }
-                
+
                 realQueue1.add(pop);
-                
+
                 sumQueue1 += pop;
                 sumQueue2 -= pop;
-                
+
                 count++;
             }else{
                 break;
@@ -63,4 +77,5 @@ class Solution {
         }
         return count;
     }
+
 }
